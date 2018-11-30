103f924ee7e4d95c51bcad7fddd5e9b195fb3d24
Mark Edmondson <github@markedmondson.me>
2018-11-30 14:33:31 +0100

    improved no need for bucket global

--- cloud-storage-to-ga/main.py
+++ cloud-storage-to-ga/main.py
@@ -12,22 +12,21 @@ from google.cloud import storage
 ACCOUNTID='123456'
 WEBPROPERTYID='UA-123456-2'
 CUSTOM_DATASOURCE_ID='data_source_id'
-BUCKET='gcs_bucket_with_source'
 FOLDER='source_folder_in_gcs_bucket'
 
 # save file to /tmp only as cloud functions only supports that to write to
-def download_gcs_file(obj, to):
+def download_gcs_file(obj, to, bucket):
     client = storage.Client()
-    bucket = client.get_bucket(BUCKET)
+    bucket = client.get_bucket(bucket)
     blob = bucket.blob(obj)
 
     blob.download_to_filename(to)
     logging.debug('downloaded file {} to {}'.format(obj, to))
 
 # needs an auth.json file as cloud auth not working for analytics requests
-def get_ga_service():
+def get_ga_service(bucket):
 
-    download_gcs_file('auth.json', '/tmp/auth.json')
+    download_gcs_file('auth.json', '/tmp/auth.json', bucket)
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
             '/tmp/auth.json',
             scopes=['https://www.googleapis.com/auth/analytics',
@@ -36,13 +35,13 @@ def get_ga_service():
     # Build the service object.
     return build('analytics', 'v3', credentials=credentials, cache_discovery=False)
 
-def upload_ga(obj_name, ga_config):
+def upload_ga(obj_name, bucket):
 
     filename = '/tmp/{}'.format(os.path.basename(obj_name))
 
-    download_gcs_file(obj_name, filename)
+    download_gcs_file(obj_name, filename, bucket)
 
-    analytics = get_ga_service()
+    analytics = get_ga_service(bucket)
 
     try:
       media = MediaFileUpload(filename,
@@ -81,9 +80,11 @@ def gcs_to_ga(data, context):
                                                                       data['timeCreated'],
                                                                       data['updated']))
     folder = FOLDER
+    object_name = data['name']
+    bucket = data['bucket']
     if object_name.startswith(folder):
       logging.info('File matches folder {}'.format(folder))
 
-      upload_ga(object_name, ga_config)
+      upload_ga(object_name, bucket)
       
     return
